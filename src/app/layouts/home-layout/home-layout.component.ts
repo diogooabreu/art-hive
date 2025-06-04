438d8afbb2f70f39fc808b0e47dcc425f9ed2882
malware171 <henrique.2004mauro@outlook.com>
2025-06-03 22:09:52 -0300

    Update home layout import

--- src/app/layouts/home-layout/home-layout.component.ts
+++ src/app/layouts/home-layout/home-layout.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../shared/footer/footer.component';
-import { ArtworksComponent } from '../../components/artworks/artworks.component';
 
 @Component({
   selector: 'app-home-layout',
