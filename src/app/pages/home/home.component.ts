ff9cc232a4a1abbe2c2afecfc8a19c4075781ae6
malware171 <henrique.2004mauro@outlook.com>
2025-06-12 20:08:19 -0300

    Add heading in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ArtworksComponent } from '../../components/artworks/artworks.component';
+import { HeadingComponent } from '../../components/heading/heading.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ArtworksComponent],
+  imports: [ArtworksComponent, HeadingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
