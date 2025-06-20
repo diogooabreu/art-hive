377304bdea505d0bb7ec3e679d3f8137aeee5e3c
Diogo Abreu <abreudiogo177@gmail.com>
2025-06-19 21:37:04 -0300

    Added order services and order model

--- src/app/components/budget-card/budget-card.component.ts
+++ src/app/components/budget-card/budget-card.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import Order from '../../Model/order';
+import { OrdersService } from '../../services/orders-service.service';
+import { NotificationsService } from '../../services/notifications-service.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-budget-card',
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './budget-card.component.html',
   styleUrl: './budget-card.component.css',
 })
-export class BudgetCardComponent {}
+export class BudgetCardComponent {
+  email = '';
+  subject = '';
+  message = '';
+
+  constructor(
+    private orderSrevice: OrdersService,
+    private notification: NotificationsService
+  ) { }
+
+  async handleSubmit() {
+    try {
+      const order = new Order(this.email, this.subject, this.message);
+      await this.orderSrevice.createOrder(order);
+      this.notification.succes("Pedido realizado com sucesso", "Sucesso");
+      this.email = this.subject = this.message = '';
+    } catch (err) {
+      this.notification.error("Erro ao fazer o pedido", "Erro");
+    }
+  }
+}
---
+++ src/app/services/orders-service.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import Order from '../Model/order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+  private baseUrl = 'https://localhost:3000/orders';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createOrder(order: Order): Promise<Order> {
+    return this.http.post<Order>(this.baseUrl, order)
+      .toPromise()
+      .then(res => res ?? this.getEmptyOrder());
+  }
+
+  getOrdersByEmail(email: string): Promise<Order[]> {
+    return this.http.get<Order[]>(`${this.baseUrl}?email=${email}`)
+      .toPromise()
+      .then(res => res ?? []);
+  }
+
+  getAllOrders(): Promise<Order[]> {
+    return this.http.get<Order[]>(this.baseUrl)
+      .toPromise()
+      .then(res => res ?? []);
+  }
+
+  private getEmptyOrder(): Order {
+    return new Order('', '', '');
+  }
+}
